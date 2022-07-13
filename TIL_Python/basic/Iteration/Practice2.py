b68408848ce99b5d2a5f44b4162c66f1f6ceb355
Parkjeongha <parkj725@naver.com>
2022-07-13 17:05:56 +0900

    Create Practice2.py

---
+++ TIL_Python/basic/Iteration/Practice2.py
@@ -0,0 +1,36 @@
+# Make a program getting numbers untill user inputs 'done'
+# Calculating max and min with inputted numbers
+# If user inputs wrong data, code should print "Invalid input"
+
+max = None
+min = None
+
+while True:
+    num = input("Enter a number:")
+    try: 
+        if num != 'done':
+            num = int(num)
+            if max == None and min == None:
+                max = num
+                min = num
+                continue
+            else:
+                if max < num:
+                    max = num
+                    continue
+                else:
+                    if min > num:
+                        min = num
+                        continue
+                    else:
+                        continue
+        elif num == 'done':
+            break
+    except:
+        if num != 'done':
+            print("Invalid output")
+            continue
+        else:
+            break
+
+print(max, min)
