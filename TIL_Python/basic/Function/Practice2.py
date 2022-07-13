5b5b074ee25dd3e3c6c42f8edcbcd41728083544
JHPark2000 <107333480+JHPark2000@users.noreply.github.com>
2022-07-13 19:18:21 +0900

    Update Practice2.py
    Calculating grade from inputted score. I wrote code two ways for avoiding wrong data.

--- TIL_Python/basic/Function/Practice2.py
+++ TIL_Python/basic/Function/Practice2.py
@@ -28,15 +28,15 @@ computegrade(a)
 def computegrade(score):
     if type(score) == str:
         grade = "Bad score"
-    elif type(score) != str and score >= 0.9 and score <= 1:
+    elif type(score) == str and score >= 0.9 and score <= 1:
         grade = "A"
-    elif score >= 0.8 and score < 0.9:
+    elif type(score) == str score >= 0.8 and score < 0.9:
         grade = "B"
-    elif score >= 0.7 and score < 0.8:
+    elif type(score) == str score >= 0.7 and score < 0.8:
         grade = "C"
-    elif score >= 0.6 and score < 0.7:
+    elif type(score) == str score >= 0.6 and score < 0.7:
         grade = "D"
-    elif score < 0.6:
+    elif type(score) == str and score < 0.6:
         grade = "F"
     else :
         grade = "Bad score"
